39b7cc2357b1dacc10c13b56990d84b0bb6d8d53
kornect <mogauisakm@live.com>
2022-08-11 21:02:32 +0200

    added initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ['Rubik'],
+      },
+      spacing: {
+        108: '27rem',
+        120: '30rem',
+        132: '33rem',
+      },
+      transformOrigin: {
+        0: '0%',
+      },
+      zIndex: {
+        '-1': '-1',
+      },
+      fontSize: {
+        xxs: ['0.60rem', { lineHeight: '1rem' }],
+      },
+    },
+  },
+  plugins: [],
+};
